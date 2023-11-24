18bfe43386811ee0f1a29e89720cf8cd56d96db9
Naufal Khairul <naufalkr394@gmail.com>
2023-11-25 01:58:31 +0700

    modified monster-player collision in entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -19,10 +20,18 @@ public class Entity {
     public BufferedImage left;
     public BufferedImage logo;
     public static String direction;
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+    public int actionLockCounter = 0;
 
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
     public int solidAreaDefaultX, solidAreaDefaultY;
     public boolean collisionOn = false;
+    
+    //VAR BUAT OBJECT
+    public BufferedImage image, image1, image2, image3, image4, image5, image6, image7;
+	public String name;
+	public boolean collision = false;
     
     //CHARACTER STATUS
     public int maxLife;
@@ -31,6 +40,79 @@ public class Entity {
     public Entity (GamePanel gp) {
     	this.gp = gp;
     }
+    
+    public void setAction() {
+    	
+    }
+    
+    public void update() {
+    	setAction();
+    	
+    	collisionOn = false;
+    	gp.cChecker.checkTile(this);
+    	gp.cChecker.checkObject(this, false);
+    	gp.cChecker.checkPlayer(this);
+    	
+    
+    	//IF COLLISION IS FALSE, PLAYER CAN MOVE
+    	if(collisionOn == false) {
+        	 switch (direction) {
+                 case "up":
+                     worldY -= speed;
+                     break;
+                 case "down":
+                     worldY += speed;
+                     break;
+                 case "left":
+                     worldX -= speed;
+                     break;
+                 case "right": 
+                     worldX += speed;
+                     break;
+             }
+        }
+    	
+    	spriteCounter++;
+    	if (spriteCounter > 12) {
+    		if(spriteCounter == 1) {
+    			spriteNum = 2;
+    		}
+    		else if(spriteNum == 2) {
+    			spriteNum = 1;
+    		}
+    		spriteCounter = 0;
+    	}
+    }
+    
+    
+    public void draw(Graphics2D g2) {
+    	BufferedImage image = null;
+    	
+    	int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+        
+        
+        switch (direction) {
+        case "":
+            image = right;
+            break;
+        case "up":
+            image = left;
+            break;
+        case "down":
+            image = right;
+            break;
+        case "left":
+            image = left;
+            break;
+        case "right":
+            image = right;
+            break;
+        }
+        
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+    }
+    
     
     public BufferedImage setup(String imagePath) {
     	UtilityTool uTool = new UtilityTool();
